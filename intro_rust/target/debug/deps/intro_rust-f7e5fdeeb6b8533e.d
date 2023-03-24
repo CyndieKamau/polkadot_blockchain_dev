25634c9c33ad8373626e613e023197e5d39187e4
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-03-24 21:49:01 +0300

    Restructured the repo

---
+++ intro_rust/target/debug/deps/intro_rust-f7e5fdeeb6b8533e.d
@@ -0,0 +1,5 @@
+/home/hp/Desktop/Eth/polkadot_blockchain_dev/intro_rust/target/debug/deps/intro_rust-f7e5fdeeb6b8533e.rmeta: src/main.rs
+
+/home/hp/Desktop/Eth/polkadot_blockchain_dev/intro_rust/target/debug/deps/intro_rust-f7e5fdeeb6b8533e.d: src/main.rs
+
+src/main.rs:
---
+++ intro_rust/target/debug/deps/intro_rust-fba06ab049510299.d
@@ -0,0 +1,5 @@
+/home/hp/Desktop/Eth/polkadot_blockchain_dev/intro_rust/target/debug/deps/intro_rust-fba06ab049510299.rmeta: src/main.rs
+
+/home/hp/Desktop/Eth/polkadot_blockchain_dev/intro_rust/target/debug/deps/intro_rust-fba06ab049510299.d: src/main.rs
+
+src/main.rs:
